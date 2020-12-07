994505decf95a392f0fd2bd40c252d54e8304620
Artem Zakharchenko <kettanaito@gmail.com>
2020-12-07 12:43:11 +0100

    Ensures "BroadcastChannel" operation in browser only

--- src/LiveStorage.ts
+++ src/LiveStorage.ts
@@ -5,52 +5,57 @@ interface BroadcastChannelMessageType<ValueType> {
   }
 }
 
+const IS_BROWSER = typeof window !== 'undefined'
+
 export class LiveStorage<ValueType> {
   public id: string
   public value: ValueType
 
-  private channel: BroadcastChannel
+  private channel: BroadcastChannel | null
 
   constructor(id: string, initialValue: ValueType) {
     this.id = id
     const hydratedValue = this.hydrateValue()
     this.value = hydratedValue || initialValue
+    this.channel = IS_BROWSER ? new BroadcastChannel(id) : null
 
-    // Create a BroadcastChannel to sync the value updates between the clients.
-    this.channel = new BroadcastChannel(id)
-    this.channel.addEventListener(
-      'message',
-      <MessageType extends keyof BroadcastChannelMessageType<ValueType>>(
-        event: MessageEvent<{
-          type: MessageType
-          payload: BroadcastChannelMessageType<ValueType>[MessageType]
-        }>
-      ) => {
-        switch (event.data.type) {
-          // Broadcast the current storage's value to all other storages
-          // in case of the hydrate request.
-          case 'HYDRATE': {
-            this.broadcastValue(this.value)
-            break
+    if (IS_BROWSER) {
+      // Create a BroadcastChannel to sync the value updates between the clients.
+      this.channel = new BroadcastChannel(id)
+      this.channel.addEventListener(
+        'message',
+        <MessageType extends keyof BroadcastChannelMessageType<ValueType>>(
+          event: MessageEvent<{
+            type: MessageType
+            payload: BroadcastChannelMessageType<ValueType>[MessageType]
+          }>,
+        ) => {
+          switch (event.data.type) {
+            // Broadcast the current storage's value to all other storages
+            // in case of the hydrate request.
+            case 'HYDRATE': {
+              this.broadcastValue(this.value)
+              break
+            }
+
+            case 'UPDATE': {
+              this.value = event.data.payload.nextValue
+
+              // Persist the synced value, because each client keeps
+              // the value in its own session storage. That way the
+              // persistency layer auto-cleans itself when a client closes.
+              this.persistValue()
+              break
+            }
           }
+        },
+      )
 
-          case 'UPDATE': {
-            this.value = event.data.payload.nextValue
-
-            // Persist the synced value, because each client keeps
-            // the value in its own session storage. That way the
-            // persistency layer auto-cleans itself when a client closes.
-            this.persistValue()
-            break
-          }
-        }
+      // When there's no hydated value for the current client,
+      // request hydration from another opened client to pull its value.
+      if (!hydratedValue) {
+        this.postMessage('HYDRATE')
       }
-    )
-
-    // When there's no hydated value for the current client,
-    // request hydration from another opened client to pull its value.
-    if (!hydratedValue) {
-      this.postMessage('HYDRATE')
     }
   }
 
@@ -66,8 +71,11 @@ export class LiveStorage<ValueType> {
   update = (updater: (prevValue: ValueType) => ValueType) => {
     const nextValue = updater(this.value)
     this.value = nextValue
-    this.persistValue()
-    this.broadcastValue(this.value)
+
+    if (IS_BROWSER) {
+      this.persistValue()
+      this.broadcastValue(this.value)
+    }
   }
 
   private serialize = (value: ValueType): string => {
@@ -82,15 +90,21 @@ export class LiveStorage<ValueType> {
    * Flush the value into the persistency layer.
    */
   private persistValue = () => {
-    sessionStorage.setItem(this.id, this.serialize(this.value))
+    if (IS_BROWSER) {
+      sessionStorage.setItem(this.id, this.serialize(this.value))
+    }
   }
 
   /**
    * Hydrate the value from the persistency layer.
    */
   private hydrateValue = (): ValueType | null => {
-    const storedValue = sessionStorage.getItem(this.id)
-    return storedValue ? this.deserialize(storedValue) : null
+    if (IS_BROWSER) {
+      const storedValue = sessionStorage.getItem(this.id)
+      return storedValue ? this.deserialize(storedValue) : null
+    }
+
+    return this.value
   }
 
   private postMessage = <
@@ -100,7 +114,7 @@ export class LiveStorage<ValueType> {
     type: MessageType,
     ...rest: MessagePayload extends never ? never : [MessagePayload]
   ) => {
-    this.channel.postMessage({
+    this.channel?.postMessage({
       type,
       payload: rest[0],
     })
